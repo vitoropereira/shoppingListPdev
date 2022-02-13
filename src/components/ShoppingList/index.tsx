a9f26489b26a7e91bf4ef02692701531ed564ae4
Vitor <vop1234@hotmail.com>
2022-02-13 03:54:25 -0300

    Firestore Realtime Database

--- src/components/ShoppingList/index.tsx
+++ src/components/ShoppingList/index.tsx
@@ -11,33 +11,21 @@ export function ShoppingList() {
   const [products, setProducts] = useState<ProductProps[]>([]);
 
   useEffect(() => {
-    firestore()
+    const subscribe = firestore()
       .collection("products")
-      .get()
-      .then((response) => {
-        const data = response.docs.map((doc) => {
+      .onSnapshot((querySnapshot) => {
+        const data = querySnapshot.docs.map((doc) => {
           return {
             id: doc.id,
             ...doc.data(),
           };
         }) as ProductProps[];
-        setProducts(data);
-      })
-      .catch((error) => console.error(error));
-  }, []);
 
-  // useEffect(() => {
-  //   firestore()
-  //     .collection("products")
-  //     .doc("my-custon-id")
-  //     .get()
-  //     .then((response) =>
-  //       console.log({
-  //         id: response.id,
-  //         ...response.data(),
-  //       })
-  //     );
-  // }, []);
+        setProducts(data);
+      });
+
+    return () => subscribe();
+  }, []);
 
   return (
     <FlatList
