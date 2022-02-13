9ad9f53393c75c8530e98a54820302a3cd8d7d60
Vitor <vop1234@hotmail.com>
2022-02-13 04:30:53 -0300

    Cloud Firestore Emulator Installation and Setup

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from "expo";
 
-import App from './App';
+import "@react-native-firebase/app";
+import firestore from "@react-native-firebase/firestore";
 
+import App from "./App";
+
+if (__DEV__) {
+    firestore().useEmulator("192.168.43.247", 8080);
+}
+
+const db = firestore();
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
